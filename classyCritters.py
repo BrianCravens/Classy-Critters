ebf041f7dd033a6e52a521b4eeb489be2a3c5217
Brian Cravens <brian.cravens303@gmail.com>
2020-07-15 14:03:32 -0500

    Initial Commit

---
+++ classyCritters.py
@@ -0,0 +1,122 @@
+from datetime import date
+
+class Lama: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+rosco = Lama("Rosco", "Lama")
+class Donkey: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+frosty = Donkey("Frosty", "Donkey")
+class Goat: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+bella = Goat("Bella", "Goat")
+class Copperhead: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+rusty = Copperhead("Rusty", "Copperhead")
+class Rat_Snake: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+ratty = Rat_Snake("Ratty", "Rat_Snake")
+class Mallard: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+daffy = Mallard("Daffy", "Mallard")
+class Goldfish: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+willy = Goldfish("Willy", "Goldfish")
+class Alligator: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+cuddles = Alligator("Cuddles", "Alligator")
+class Monkey: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+bill = Monkey("Bill", "Monkey")
+class Lion: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+nasty = Lion("Nasty", "Lion")
+class Anaconda: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+tiny = Anaconda("Tiny", "Anaconda")
+class Python: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+hugs = Python("Hugs", "Python")
+class King_Cobra: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+nebuchadnezzar = King_Cobra("Nebuchadnezzar", "King Cobra")
+class Catfish: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+tricks = Catfish("Tricks", "Catfish")
+class Frog: 
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+froggy = Frog("Froggy", "Frog")
